858dfc856cef132c2e0af956d46e0315f8d5145a
hunyong-song <hunyong.song@nhn.com>
2016-10-17 21:57:39 +0900

    부모 컴포넌트에서 자식 컴포넌트로 자료 전달

--- js/src/app.js
+++ js/src/app.js
@@ -6,13 +6,20 @@ var AddLi = require('./component/AddLi');
 var TodoLi = require('./component/TodoLi');
 var EditLi = require('./component/EditLi');
 
+var todos = [
+    '이것도 해야 되고',
+    '저것도 해야 되고',
+    '그것도 해야 되고',
+    '언제 다하나'
+];
+
 ReactDOM.render(
     <div className="container">
         <Title />
         <AddLi />
         <hr/>
         <ul>
-            <TodoLi />
+            <TodoLi todos={todos} />
             <EditLi />
         </ul>
     </div>,
--- js/src/component/TodoLi.js
+++ js/src/component/TodoLi.js
@@ -1,15 +1,8 @@
 var React = require('react');
 
-var todos = [
-    '이것도 해야 되고',
-    '저것도 해야 되고',
-    '그것도 해야 되고',
-    '언제 다하나'
-];
-
 var TodoLi = React.createClass({
     render: function () {
-        var todoLi = todos.map(function (todo, i) {
+        var todoLi = this.props.todos.map(function (todo, i) {
             return (
                 <li key={"todo" + i}>
                     <span>{todo}</span>
