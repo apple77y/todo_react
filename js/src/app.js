5bd195dceca53b028cca3601da8cd616b6f98564
hunyong-song <hunyong.song@nhn.com>
2016-10-17 21:46:29 +0900

    AddLi 컴포넌트 생성

--- js/src/app.js
+++ js/src/app.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Title = require('./component/title');
+var AddLi = require('./component/addLi');
 
 var todos = [
     '이것도 해야 되고',
@@ -9,15 +10,6 @@ var todos = [
     '그것도 해야 되고',
     '언제 다하나'
 ];
-
-var addLi = (
-    <div className="input-group input-group-lg">
-        <input type="text" className="form-control" placeholder="할 일을 입력해주세요"/>
-        <span className="input-group-btn">
-            <button className="btn btn-primary" type="button">등록</button>
-        </span>
-    </div>
-);
 
 var todoLi = todos.map(function (todo, i) {
     return (
@@ -40,7 +32,7 @@ var editLi = (
 ReactDOM.render(
     <div className="container">
         <Title />
-        {addLi}
+        <AddLi />
         <hr/>
         <ul>
             {todoLi}
