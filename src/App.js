23c4362b59d87e0b6ef2efbe1b8bb971f377910c
hunyong-song <hunyong.song@nhn.com>
2016-10-17 21:51:08 +0900

    TodoLi 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Title = require('./component/title');
-var AddLi = require('./component/addLi');
-
-var todos = [
-    '이것도 해야 되고',
-    '저것도 해야 되고',
-    '그것도 해야 되고',
-    '언제 다하나'
-];
-
-var todoLi = todos.map(function (todo, i) {
-    return (
-        <li key={"todo" + i}>
-            <span>{todo}</span>
-            <span className="btn-container"><a href="#">삭제</a></span>
-        </li>
-    );
-});
+var Title = require('./component/Title');
+var AddLi = require('./component/AddLi');
+var TodoLi = require('./component/TodoLi');
+var EditLi = require('./component/EditLi');
 
 ReactDOM.render(
     <div className="container">
@@ -26,7 +12,8 @@ ReactDOM.render(
         <AddLi />
         <hr/>
         <ul>
-            {todoLi}
+            <TodoLi />
+            <EditLi />
         </ul>
     </div>,
 
