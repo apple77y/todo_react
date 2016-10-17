a312b3fc897a080b5dc56ae70e9d64e02c59e397
hunyong-song <hunyong.song@nhn.com>
2016-10-17 22:11:23 +0900

    getInitialState 함수를 통해 최초 데이터 값을 state로 갖게 됨

--- js/src/container/index.js
+++ js/src/container/index.js
@@ -5,24 +5,27 @@ var AddLi = require('../component/AddLi');
 var TodoLi = require('../component/TodoLi');
 var EditLi = require('../component/EditLi');
 
-var text = 'To do list';
-
-var todos = [
-    '이것도 해야 되고',
-    '저것도 해야 되고',
-    '그것도 해야 되고',
-    '언제 다하나'
-];
-
 var Container = React.createClass({
+    getInitialState: function () {
+        return {
+            text: 'To do list',
+            todos: [
+                '이것도 해야 되고',
+                '저것도 해야 되고',
+                '그것도 해야 되고',
+                '언제 다하나'
+            ]
+        };
+    },
+
     render: function () {
         return (
             <div className="container">
-                <Title text={text} />
+                <Title text={this.state.text} />
                 <AddLi />
                 <hr/>
                 <ul>
-                    <TodoLi todos={todos} />
+                    <TodoLi todos={this.state.todos} />
                     <EditLi />
                 </ul>
             </div>
