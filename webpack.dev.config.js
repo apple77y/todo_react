458ff8de5a40ec32b92c9c535bd082a2cc19f26d
hunyong-song <hunyong.song@nhn.com>
2016-10-18 19:56:36 +0900

    개발용을 위한 웹팩 devserver 설정 파일

---
+++ webpack.dev.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './js/src/app.js',
+
+    output: {
+        path: '/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        hot: true,
+        inline: true,
+        filename: 'bundle.js',
+        publicPath: '/js/dist',
+        contentBase: './'
+    },
+
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react'],
+                    plugins:['react-hot-loader/babel']
+                }
+            }
+        ]
+    }
+};
