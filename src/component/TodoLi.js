37d7129f1eb63864c2f7b0aac7131dffa5073c11
hunyong-song <hunyong.song@nhn.com>
2016-10-18 10:32:04 +0900

    반복문 행위를 컴포넌트가 아닌 컨테이너에서 하도록 리팩토링

--- src/component/TodoLi.js
+++ src/component/TodoLi.js
@@ -3,19 +3,12 @@ var React = require('react');
 var TodoLi = React.createClass({
 
     render: function () {
-        var todoLi = this.props.todos.map(function (todo, i) {
-            return (
-                <li key={"todo" + i}>
-                    <span>{todo}</span>
-                    <span className="btn-container"><a href="#">수정</a><a href="#">삭제</a></span>
-                </li>
-            );
-        });
 
         return (
-            <div>
-                {todoLi}
-            </div>
+            <li>
+                <span>{this.props.todo}</span>
+                <span className="btn-container"><a href="#">수정</a><a href="#">삭제</a></span>
+            </li>
         );
     }
 });
