36b6a075b6a530bd7f35237fdc530b6590f99b7f
hunyong-song <hunyong.song@nhn.com>
2016-10-18 10:55:58 +0900

    삭제 버튼 클릭 이벤트 발생시 상위 컴포넌트로 데이터 전달

--- js/src/component/TodoLi.js
+++ js/src/component/TodoLi.js
@@ -1,12 +1,16 @@
 var React = require('react');
 
 var TodoLi = React.createClass({
+    onClickRemove: function () {
+        this.props.handleRemovedData(this.props.todo);
+    },
+
     render: function () {
 
         return (
             <li>
                 <span>{this.props.todo}</span>
-                <span className="btn-container"><a href="#">수정</a><a href="#">삭제</a></span>
+                <span className="btn-container"><a href="#">수정</a><a href="#" onClick={this.onClickRemove}>삭제</a></span>
             </li>
         );
     }
