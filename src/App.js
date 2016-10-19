5509d32e4f7ee2ef6451eb6bdbacf4d37415eae9
hunyong-song <hunyong.song@nhn.com>
2016-10-19 11:53:40 +0900

    초기 상태로 원복

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var title = 'To do list';
+var todos = [
+    '이것도 해야 되고',
+    '저것도 해야 되고',
+    '그것도 해야 되고',
+    '언제 다하나'
+];
+
+var title = (
+    <div className="page-header">
+        <h1>{title}</h1>
+    </div>
+);
+
+var addLi = (
+    <div className="input-group input-group-lg">
+        <input type="text" className="form-control" placeholder="할 일을 입력해주세요"/>
+        <span className="input-group-btn">
+            <button className="btn btn-primary" type="button">등록</button>
+        </span>
+    </div>
+);
+
+var todoLi = todos.map(function (todo, i) {
+    return (
+        <li key={"todo" + i}>
+            <span>{todo}</span>
+            <span className="btn-container"><a href="#">수정</a><a href="#">삭제</a></span>
+        </li>
+    );
+});
+
+var editLi = (
+    <li className="input-group input-group-lg">
+        <input type="text" className="form-control" placeholder="수정 버튼 누르면 이렇게"/>
+        <span className="input-group-btn">
+            <button className="btn btn-primary" type="button">수정완료</button>
+        </span>
+    </li>
+);
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <div className="container">
+        {title}
+        {addLi}
+        <hr/>
+        <ul>
+            {todoLi}
+            {editLi}
+        </ul>
+    </div>,
+
     document.getElementById('app')
 );
