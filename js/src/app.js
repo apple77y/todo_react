4ff0159c685b68456a2ae62eb88d3b4ee3ca2af5
hunyong-song <hunyong.song@nhn.com>
2016-10-19 12:23:41 +0900

    엔트리 포인트 위치 변경

--- js/src/app.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var Container = require('./container/index');
-
-ReactDOM.render(
-    <Container /> ,
-    document.getElementById('app')
-);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Title = require('./component/Title');
-var AddLi = require('./component/AddLi');
-var TodoLi = require('./component/TodoLi');
+var Container = require('./container/index');
 
 if (module.hot) {
     module.hot.accept();
 }
 
 ReactDOM.render(
-    <div className="container">
-        <Title />
-        <AddLi />
-        <hr/>
-        <ul>
-            <TodoLi />
-        </ul>
-    </div>,
+    <Container />,
 
     document.getElementById('app')
 );
