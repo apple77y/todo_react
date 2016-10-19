3abc872b7f9b5c0315dc23bb3ed12abb6cf0efeb
hunyong-song <hunyong.song@nhn.com>
2016-10-19 12:19:02 +0900

    컴포넌트의 위치 변경

--- src/component/TodoLi.js
+++ src/component/TodoLi.js
@@ -1,19 +1,28 @@
 var React = require('react');
 
+var todos = [
+    '이것도 해야 되고',
+    '저것도 해야 되고',
+    '그것도 해야 되고',
+    '언제 다하나'
+];
+
 var TodoLi = React.createClass({
-    onClickRemove: function () {
-        this.props.handleRemovedData(this.props.todo);
-    },
 
     render: function () {
+        var todoLi = todos.map(function (todo, i) {
+            return (
+                <li key={"todo" + i}>
+                    <span>{todo}</span>
+                    <span className="btn-container"><a href="#">수정</a><a href="#">삭제</a></span>
+                </li>
+            );
+        });
 
         return (
-            <li>
-                <span>{this.props.todo}</span>
-                <span className="btn-container">
-                    <a href="#" onClick={this.onClickRemove}>삭제</a>
-                </span>
-            </li>
+            <div>
+                {todoLi}
+            </div>
         );
     }
 });
