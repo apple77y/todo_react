11e2efbc01dec943f07d2bca95a5d158e06fea81
hunyong-song <hunyong.song@nhn.com>
2016-10-19 12:19:02 +0900

    컴포넌트의 위치 변경

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,11 +1,16 @@
 var React = require('react');
 
-var Title = function (props) {
-    return (
-        <div className="page-header">
-            <h1>{props.text}</h1>
-        </div>
-    );
-};
+var text = 'To do list';
+
+var Title = React.createClass({
+
+    render: function () {
+        return (
+            <div className="page-header">
+                <h1>{text}</h1>
+            </div>
+        );
+    }
+});
 
 module.exports = Title;
