cd7d50d7c021df830562f7cf3ff2963f5c9fd546
hunyong-song <hunyong.song@nhn.com>
2016-10-20 18:34:22 +0900

    순수 함수로 리팩토링

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,16 +1,11 @@
 var React = require('react');
 
-var text = 'To do list';
-
-var Title = React.createClass({
-
-    render: function () {
-        return (
-            <div className="page-header">
-                <h1>{text}</h1>
-            </div>
-        );
-    }
-});
+var Title = function(props) {
+    return (
+        <div className="page-header">
+            <h1>{props.text}</h1>
+        </div>
+    );
+};
 
 module.exports = Title;
