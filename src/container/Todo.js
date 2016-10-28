bd917801719c756a72086c4f13106f384f7a89e7
hunyong-song <hunyong.song@nhn.com>
2016-10-29 03:45:53 +0900

    container 이름 변경

---
+++ src/container/Todo.js
@@ -0,0 +1,81 @@
+var React = require('react');
+var connect = require('react-redux').connect;
+
+var Title = require('../component/Title');
+var AddLi = require('../component/AddLi');
+var TodoLi = require('../component/TodoLi');
+
+var getData = require('../action/todo').getData;
+var addTodo = require('../action/todo').addTodo;
+var removeTodo = require('../action/todo').removeTodo;
+
+var Todo = React.createClass({
+    defaultProps: function () {
+        return {
+            text: '',
+            todos: []
+        };
+    },
+
+    componentDidMount: function () {
+        this.props.onGetData();
+    },
+
+    handleAddedData: function (todo) {
+        if (!todo.trim()) {
+            return false;
+        }
+
+        this.props.onAddTodo(todo);
+    },
+
+    handleRemovedData: function (todo) {
+        this.props.onRemoveTodo(todo);
+    },
+
+    render: function () {
+        var self = this;
+        var todoLi = this.props.todos.map(function (todo, i) {
+            return <TodoLi todo={todo} key={'todo' + i} handleRemovedData={self.handleRemovedData}/>;
+        });
+
+        return (
+            <div className="container">
+                <Title text={this.props.text}/>
+                <AddLi handleAddedData={this.handleAddedData}/>
+                <hr/>
+                <ul>
+                    {todoLi}
+                </ul>
+            </div>
+        );
+    }
+});
+
+Todo.propTypes = {
+    text: React.PropTypes.string.isRequired,
+    todos: React.PropTypes.array.isRequired
+};
+
+var mapStateToProps = function (state) {
+    return {
+        text: state.text,
+        todos: state.todos
+    }
+};
+
+var mapDispatchToProps = function (dispatch) {
+    return {
+        onGetData: function () {
+            dispatch(getData());
+        },
+        onAddTodo: function (todo) {
+            dispatch(addTodo(todo));
+        },
+        onRemoveTodo: function (todo) {
+            dispatch(removeTodo(todo));
+        }
+    }
+};
+
+module.exports = connect(mapStateToProps, mapDispatchToProps)(Todo);
