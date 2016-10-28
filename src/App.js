86c5d03343992f0e0bc3f58c870485d3322c33cc
hunyong-song <hunyong.song@nhn.com>
2016-10-29 03:50:14 +0900

    스토어 바인딩

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Redux = require('redux');
+var ReactRedux = require('react-redux');
+var createStore = Redux.createStore;
+var Provider = ReactRedux.Provider;
 
-var Container = require('./container/index');
+var reducer = require('./reducer/todo');
+var Todo = require('./container/Todo');
+var store = createStore(reducer);
 
 if (module.hot) {
     module.hot.accept();
 }
 
 ReactDOM.render(
-    <Container />,
-
+    <Provider store={store}>
+        <Todo />
+    </Provider>,
     document.getElementById('app')
 );
