ad1403e22628cefd0012c80801223e9b74ada049
Hun-yong Song <apple77y@gmail.com>
2017-07-15 17:25:40 +0900

    HelloWorld 완성

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+ReactDOM.render(
+    <div>Hello World</div>, document.getElementById("app")
+);
