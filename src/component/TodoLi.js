214e335389b128e21dc484f4326027e2e628228e
hunyong.song <hunyong.song@nhn.com>
2017-07-15 18:37:59 +0900

    투두 추가, 투두 삭제 기능 추가

--- src/component/TodoLi.js
+++ src/component/TodoLi.js
@@ -1,13 +1,21 @@
 import React, {Component} from "react";
 
 class TodoLi extends Component {
+    constructor() {
+        super();
+        this.onClickRemoveButton = this.onClickRemoveButton.bind(this);
+    }
     render() {
         return (
             <li>
                 <span>{this.props.todo}</span>
-                <span className="btn-container"><a href="#">삭제</a></span>
+                <span className="btn-container"><a href="#" onClick={this.onClickRemoveButton}>삭제</a></span>
             </li>
         );
+    }
+
+    onClickRemoveButton() {
+        this.props.handleRemovedData();
     }
 }
 
